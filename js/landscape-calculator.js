90acdb9f05cd3314b0b087c95321302314725654
Kevin Henneberger <henneberger@g.harvard.edu>
2013-12-01 17:13:53 -0500

    first attempt at combining front & back totals

--- js/landscape-calculator.js
+++ js/landscape-calculator.js
@@ -5,13 +5,14 @@ $('button').click(front_calculate);
 $('input,select').change(front_calculate);
 $('button').click(back_calculate);
 $('input,select').change(back_calculate);
+$('button').click(total_calculate);
+$('input,select').change(total_calculate);     
 
 /*-------------------------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------------------------*/
 function front_calculate() {
     
-    var budget              = $('#budget').val();
     var front_size          = $('#front_size').val();
     var front_services      = $('input[name=front_services]:checked');
     var front_total         = 0;
@@ -25,21 +26,19 @@ function front_calculate() {
             
             front_total = front_total + amount;
             
-    }); // end of loop
+    });
     
     $('#front_output').html(front_total);
     
 }
-
 /*-------------------------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------------------------*/
 function back_calculate() {
     
-    var budget              = $('#budget').val();
     var back_size           = $('#back_size').val();
     var back_services       = $('input[name=back_services]:checked');
-    var back_total         = 0;
+    var back_total          = 0;
 
     
     back_services.each(function() {
@@ -50,8 +49,36 @@ function back_calculate() {
             
             back_total = back_total + amount;
             
-    }); // end of loop
+    });
     
     $('#back_output').html(back_total);
     
 }        
+/*-------------------------------------------------------------------------------------------------
+BROKEN: need to successfully add front_output and back_output together:
+-------------------------------------------------------------------------------------------------*/
+function total_calculate() {
+
+    var front_output     = $('front_output]').val();
+    var back_output      = $('back_output]').val();
+
+    total_output = front_output + back_output
+
+    $('#total_output').html(total_output);
+    
+}
+/*-------------------------------------------------------------------------------------------------
+
+
+function budget_calculate() {
+
+    var budget           = $('#budget').val();
+    var total_output     = $('total_output]').val();
+
+    remaining_budget = budget - total_output
+
+    $('#remaining_budget').html(remaining_budget);
+    
+}
+
+-------------------------------------------------------------------------------------------------*/
